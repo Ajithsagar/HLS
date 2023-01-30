60f7d2d59416fcfe4a90c6121910b4e3ae44b764
Ajith Sagar Z <47409429+Ajithsagar@users.noreply.github.com>
2023-01-30 13:12:57 +0530

    Create m3u8_client.c

---
+++ m3u8_client.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main () 
+{
+  
+int socket_desc;
+  
+struct sockaddr_in server_addr;
+  
+char buffer[1024];
+  
+int file_size;
+  
+ 
+    // Create a socket
+    if ((socket_desc = socket (AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+      
+perror ("socket");
+      
+return 1;
+    
+}
+  
+    // Set socket address
+    memset (&server_addr, 0, sizeof (server_addr));
+  
+server_addr.sin_family = AF_INET;
+  
+server_addr.sin_addr.s_addr = inet_addr ("127.0.0.1");
+  
+server_addr.sin_port = htons (1234);
+  
+ 
+// Connect to the server
+    if (connect
+	(socket_desc, (struct sockaddr *) &server_addr,
+	 sizeof (server_addr)) < 0)
+    {
+      
+perror ("connect");
+      
+return 1;
+    
+}
+  
+ 
+// Read the file size from the server
+    recv (socket_desc, buffer, sizeof (buffer), 0);
+  
+sscanf (buffer, "%d", &file_size);
+  
+ 
+// Open a local file to save the .m3u8 file
+    FILE * file = fopen ("output1.m3u8", "wb");
+  
+if (!file)
+    {
+      
+perror ("fopen");
+      
+return 1;
+    
+}
+  
+puts("Requesting M3U8 file "); 
+// Receive the file contents from the server
+  int bytes_received = 0;
+  
+while (bytes_received < file_size)
+    {
+      
+int n = recv (socket_desc, buffer, sizeof (buffer), 0);
+      
+fwrite (buffer, 1, n, file);
+      
+bytes_received += n;
+    
+}
+puts("M3U8 file recieved");  
+ 
+// Close the file and the socket
+    fclose (file);
+  
+close (socket_desc);
+  
+ 
+return 0;
+  
+}
